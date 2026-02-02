24cd0aa6ebfd96006c4ed291487b711e2222c2d3
mahesh <mahesh050917@gmail.com>
2026-02-02 20:54:56 +0530

    ec2 creation

--- 01.ec2-creation.sh
+++ 01.ec2-creation.sh
@@ -2,15 +2,27 @@
 
 # Create ec2 instance using AWS CLI
 
-Amiid=ami-0220d79f3f480ecf5 
+AmiId=ami-0220d79f3f480ecf5 
 Instancetype="t3.micro"
 SGID="sg-018e4789272ba8904"
-Name="Frontend"
 subnetid="subnet-06a0137ee419d9703"
 
-aws ec2 run-instances --image-id $Amiid \
---instance-type $Instancetype \
-    --subnet-id $subnetid \
-    --security-group-ids $SGID  \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$Name}]"
+for Instance in $@
+do
 
+Instance_ID=$(aws ec2 run-instances --image-id $AmiId \
+              --instance-type $Instancetype \
+              --subnet-id $subnetid \
+              --security-group-ids $SGID  \
+              --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$Instance}]" \
+              --query 'Instances[0].InstanceId' --output text)
+
+echo "Provide the InstanceID: $Instance_ID"
+
+PrivateIP=$(aws ec2 describe-instances --instance-ids $Instance_ID \
+    --query 'Reservations[*].Instances[*].PrivateIpAddress' \
+    --output text)
+
+echo "Print private IP: $PrivateIP"
+
+done
