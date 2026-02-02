b9e646dddccadd2b8277e7de80764e08dfc1ad02
mahesh <mahesh050917@gmail.com>
2026-02-02 20:34:57 +0530

    ec2 creation

---
+++ 01.ec2-creation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create ec2 instance using AWS CLI
+
+Amiid=ami-0220d79f3f480ecf5 
+Instancetype="t3.micro"
+SGID="sg-018e4789272ba8904"
+Name="Frontend"
+
+aws ec2 run-instances --image-id $Amii \
+--instance-type $Instancetype \
+    --subnet-id <subnet-id> \
+    --security-group-ids $SGID  \
+    --tag-specifications "'ResourceType=instance,Tags=[{Key=Name,Value=$Name}]'"
+
