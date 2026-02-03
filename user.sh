a06022c7752e2ae774d9b2baf2d106e64345da24
mahesh <mahesh050917@gmail.com>
2026-02-03 13:53:13 +0530

    roboshop project scripts

--- user.sh
+++ user.sh
@@ -3,14 +3,17 @@
 Userid=$(id -u)
 LOG_FOLDER=/var/log/ShellScript
 LOG_FILE=/var/log/ShellScript/$0.log
-DIR=/home/ec2-user/RoboShop-ShellScript
-MONGO_HOST=mongodb.daws88s.shop
-
+DIR=$PWD
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
 
 
 if [ $Userid -ne 0 ]; then
  
-   echo "please run the script with root access: $0" | tee -a $0.log
+   echo -e "$Y please run the script with root access: $0" 
    exit 1
 fi
 
@@ -19,9 +22,9 @@ mkdir -p $LOG_FOLDER
 VALIDATE() {
     if [ $? -ne 0 ]; then
      
-     echo "$2....Failure"
+     echo "$2....$R Failure $N"
     else
-     echo "$2....Success"
+     echo "$2....$G Success $N"
      fi
 }
 
