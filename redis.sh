6ed20a3c2b7d3fe29024a88e95be1ba2aeee4901
mahesh <mahesh050917@gmail.com>
2026-02-03 13:40:52 +0530

    roboshop project scripts

--- redis.sh
+++ redis.sh
@@ -3,14 +3,18 @@
 Userid=$(id -u)
 LOG_FOLDER=/var/log/ShellScript
 LOG_FILE=/var/log/ShellScript/$0.log
-DIR=/home/ec2-user/RoboShop-ShellScript
-
+DIR=$PWD
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
 
 
 
 if [ $Userid -ne 0 ]; then
  
-   echo "please run the script with root access: $0" | tee -a $0.log
+   echo -e "$R please run the script with root access: $0"
    exit 1
 fi
 
@@ -19,9 +23,9 @@ mkdir -p $LOG_FOLDER
 VALIDATE() {
     if [ $? -ne 0 ]; then
      
-     echo "$2....Failure"
+     echo -e "$2....$R Failure $N"
     else
-     echo "$2....Success"
+     echo -e "$2....$G Success $N"
      fi
 }
 
@@ -34,7 +38,8 @@ VALIDATE $? "Enable module redis 7"
 dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Install redis 7"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf &>>$LOG_FILE
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Allow remote Connections"
 
 systemctl enable redis &>>$LOG_FILE
 VALIDATE $? "enable systemctl redis service"
